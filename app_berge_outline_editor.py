a6ec643749983bb6a71b604b26b3193161e0d5b7
J M <wohaste@gmail.com>
2025-11-09 17:50:22 -0800

    select a filename to open

--- app_berge_outline_editor.py
+++ app_berge_outline_editor.py
@@ -1,4 +1,5 @@
 import tkinter
+import tkinter.filedialog
 import re
 import os
 import subprocess
@@ -11,14 +12,21 @@ class Window(tkinter.Frame):
         self.master = master # this was "root" in the outer code
         self.master.title("Berge Outline Editor") #title displayed on window
         self.pack(fill=tkinter.BOTH, expand=1) #layout of window
+        self.current_filename = None
         self.init_layout()
 
     def init_layout(self):
-        filename = "temp.md"
         
-        label_filename = tkinter.Label(self, text="Filename: " + filename)
+        button_frame_top  =  tkinter.Frame(self,  width=200,  height=400)
+        button_frame_top.pack(side=tkinter.TOP,  fill=tkinter.X, expand=True,  padx=10)        
+        
+        label_filename = tkinter.Label(button_frame_top, text="Filename: none")
         label_filename.pack(side=tkinter.TOP, anchor='w', padx=10, pady=5)
+        self.label_filename = label_filename
         
+        open_button = tkinter.Button(button_frame_top, text="Open File", command=self.client_open_file)
+        open_button.pack(side=tkinter.RIGHT, padx=10)
+
         textbox = tkinter.Text(self, width=90, height=10) #measured in characters
         textbox.pack(side=tkinter.TOP, fill=tkinter.X, expand=True, anchor='w', padx=10)
         textbox.focus_set()
@@ -29,9 +37,23 @@ class Window(tkinter.Frame):
         
         quitButton = tkinter.Button(button_frame_bottom, text="Quit", command=self.client_quit)
         quitButton.pack(side=tkinter.RIGHT, padx=10)
+        
+    def update_label_filename(self):
+        if self.current_filename == None:
+            self.label_filename['text'] = "Filename: None"
+        else:
+            self.label_filename['text'] = "Filename: " + os.path.basename(self.current_filename)
             
     def client_quit(self):
         exit()
+        
+    def client_open_file(self):
+        filename = tkinter.filedialog.askopenfilename()
+        if filename == (): #empty tuple
+            filename = None
+        self.current_filename = filename
+        self.update_label_filename()
+
 		
 if __name__ == "__main__":
     root = tkinter.Tk()
