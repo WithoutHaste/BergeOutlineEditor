765f8ba2f4d9b44dd0a53696d4a6b908a8bd25b1
J M <wohaste@gmail.com>
2025-11-10 19:31:20 -0800

    parse file

--- cls_file_format.py
+++ cls_file_format.py
@@ -8,26 +8,32 @@ class FileFormat():
     # will do its best to parse the file - will keep a list of errors
     def __init__(self, raw_text):
         self.parsing_errors = []
-        self.file_sections = []
+        self.file_sections = [] # just the top-level sections
+        current_file_section = None
         lines = raw_text.split('\n')
         for line in lines:
-            if line.startswith(MARKER_DUPLICATE_SECTION):
+            if line.startswith(FileFormat.MARKER_DUPLICATE_SECTION):
                 break # ignore entire duplicate section, nothing comes after this
-            level = FIleSection.get_level(line)
-            current_level = len(self.file_sections)
-            if level == 0 and current_level == 0:
-                continue # skip everything before Markdown headers start
+            level = FileSection.get_level(line)
             if level == 0:
-                self.file_sections[-1].append(line) # normal line
+                if current_file_section == None:
+                     # skip everything before Markdown headers start
+                    continue
+                # normal line
+                current_file_section.append_line(line)
                 continue
-            if level == current_level:
-                # TODO - new section attached to parent
+            while current_file_section != None and level <= current_file_section.level:
+                current_file_section = current_file_section.parent
+            if current_file_section == None:
+                # new top-level section
+                current_file_section = FileSection(None)
+                self.file_sections.append(current_file_section)
                 continue
-            if level > current_level:
-                # TODO append new child section
-                continue
-            if level < current_level:
-                # TODO pop up to correct parent level before adding new section
+            if level > current_file_section.level:
+                if level > current_file_section.level + 1:
+                    self.parsing_errors.append("Header levels skipped a step - this may not be a Berge-formatted file")
+                # add new child to parent
+                current_file_section = FileSection(current_file_section)
                 continue
             self.parsing_errors.append("Should not reach this point - unknown section depth")
         
@@ -35,12 +41,21 @@ class FileFormat():
         return len(self.parsing_errors) == 0
 
 class FileSection():
-    def __init__(self, first_line):
+    def __init__(self, parent_section):
+        self.children = []
         self.lines = []
+        self.level = 1
+        if parent_section != None:
+            self.level = parent_section.level + 1
+            parent_section.append_child(self)
+        self.parent = parent_section
         pass
         
     def append_line(self, line):
         self.lines.append(line)
+        
+    def append_child(self, child):
+        self.children.append(child)
         
     @staticmethod
     def get_level(line):
--- test_cls_file_format.py
+++ test_cls_file_format.py
@@ -1,6 +1,69 @@
 import unittest
 from cls_file_format import *
 
+class Test_FileFormat(unittest.TestCase):
+    def test_init(self):
+        # no headers
+        result = FileFormat("abc\ndef")
+        self.assertEqual(len(result.file_sections), 0)
+        self.assertEqual(len(result.parsing_errors), 0)
+        # one header
+        result = FileFormat("abc\ndef\n# A\nghi")
+        self.assertEqual(len(result.file_sections), 1)
+        self.assertEqual(result.file_sections[0].lines, ['ghi'])
+        self.assertEqual(len(result.parsing_errors), 0)
+        # one set of nested headers
+        result = FileFormat("# A\nabc\n## A.A\ndef\n### A.A.A\nghi")
+        self.assertEqual(len(result.file_sections), 1)
+        self.assertEqual(result.file_sections[0].lines, ['abc'])
+        self.assertEqual(len(result.file_sections[0].children), 1)
+        self.assertEqual(result.file_sections[0].children[0].lines, ['def'])
+        self.assertEqual(len(result.file_sections[0].children[0].children), 1)
+        self.assertEqual(result.file_sections[0].children[0].children[0].lines, ['ghi'])
+        self.assertEqual(len(result.file_sections[0].children[0].children[0].children), 0)
+        self.assertEqual(len(result.parsing_errors), 0)
+        # multiple sets of nested headers
+        result = FileFormat("# A\nabc\n## A.A\ndef\n# B\nghi\n## B.A\njkl")
+        self.assertEqual(len(result.file_sections), 2)
+        self.assertEqual(result.file_sections[0].lines, ['abc'])
+        self.assertEqual(len(result.file_sections[0].children), 1)
+        self.assertEqual(result.file_sections[0].children[0].lines, ['def'])
+        self.assertEqual(len(result.file_sections[0].children[0].children), 0)
+        self.assertEqual(result.file_sections[1].lines, ['ghi'])
+        self.assertEqual(len(result.file_sections[1].children), 1)
+        self.assertEqual(result.file_sections[1].children[0].lines, ['jkl'])
+        self.assertEqual(len(result.file_sections[1].children[0].children), 0)
+        self.assertEqual(len(result.parsing_errors), 0)
+        # jump back multiple levels AND add multiple children
+        result = FileFormat("# A\nabc\n## A.A\ndef\n### A.A.A\nghi\n#### A.A.A.A\njkl\n## A.B\nmno")
+        self.assertEqual(len(result.file_sections), 1)
+        self.assertEqual(result.file_sections[0].lines, ['abc'])
+        self.assertEqual(len(result.file_sections[0].children), 2)
+        self.assertEqual(result.file_sections[0].children[0].lines, ['def'])
+        self.assertEqual(len(result.file_sections[0].children[0].children), 1)
+        self.assertEqual(result.file_sections[0].children[0].children[0].lines, ['ghi'])
+        self.assertEqual(len(result.file_sections[0].children[0].children), 1)
+        self.assertEqual(result.file_sections[0].children[0].children[0].children[0].lines, ['jkl'])
+        self.assertEqual(len(result.file_sections[0].children[0].children[0].children[0].children), 0)
+        self.assertEqual(result.file_sections[0].children[1].lines, ['mno'])
+        self.assertEqual(len(result.file_sections[0].children[1].children), 0)
+        self.assertEqual(len(result.parsing_errors), 0)
+        # warns about skipped levels
+        result = FileFormat("# A\nabc\n### A.A.A\ndef")
+        self.assertEqual(len(result.file_sections), 1)
+        self.assertEqual(result.file_sections[0].lines, ['abc'])
+        self.assertEqual(len(result.file_sections[0].children), 1)
+        self.assertEqual(result.file_sections[0].children[0].lines, ['def'])
+        self.assertEqual(len(result.file_sections[0].children[0].children), 0)
+        self.assertEqual(len(result.parsing_errors), 1)
+        # ignore everything after duplicate section
+        result = FileFormat("# A\nabc\n# Duplicate: All Final Sections\ndef\n## A.A\nghi")
+        self.assertEqual(len(result.file_sections), 1)
+        self.assertEqual(result.file_sections[0].lines, ['abc'])
+        self.assertEqual(len(result.file_sections[0].children), 0)
+        self.assertEqual(len(result.parsing_errors), 0)
+        
+
 class Test_FileSection(unittest.TestCase):
     def test_get_level(self):
         self.assertEqual(FileSection.get_level("abcdefg"), 0)
